8e5b665d9c04741451ce85cbc6b3a03dd9eece44
cholpon22 <cholpon.omuralieva@mediaagility.com>
2024-03-04 21:58:27 -0600

    gs

--- providers.tf
+++ providers.tf
@@ -1,26 +1,47 @@
-terraform {
-  required_version = ">=0.12"
+# terraform {
+#   required_version = ">=0.12"
 
+#   required_providers {
+#     azapi = {
+#       source  = "azure/azapi"
+#       version = "~>1.5"
+#     }
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>2.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "~>3.0"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+
+#   subscription_id = "e51129db-8114-4b4e-82f0-a5b20753a623"
+#   tenant_id       = "5998536d-86eb-4576-800d-71ef0638d58a"
+# #   client_id       = ""
+# }
+
+# Define Terraform provider
+terraform {
+  required_version = ">= 1.3"
+  backend "azurerm" {
+    resource_group_name  = "kopicloud-tfstate-rg"
+    storage_account_name = "kopicloudiactest"
+    container_name       = "core-tfstate"
+    key                  = "actions.tfstate"
+  }
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~>1.5"
-    }
     azurerm = {
+      version = "~>3.2"
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
     }
   }
 }
-
-provider "azurerm" {
-  features {}
-
-  subscription_id = "e51129db-8114-4b4e-82f0-a5b20753a623"
-  tenant_id       = "5998536d-86eb-4576-800d-71ef0638d58a"
-#   client_id       = ""
+# Configure the Azure provider
+provider "azurerm" { 
+  features {}  
 }
